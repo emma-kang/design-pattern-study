2bed1bae6b25bbb6e5ad0c6875f9d3988eebf008
Emma Kang <emma140626@gmail.com>
2022-07-24 23:14:18 -0400

    signleton

--- README.md
+++ README.md
@@ -4,5 +4,5 @@ The repository is to learn and practice Design patterns.
 To understand core design patterns via practice, I will write code based on the reference book. 
 
 ### Reference
-- Freeman, E., &amp; Freeman, E. (2005). Head first design patterns. Sebastopol, CA: O'Reilly Media.
+- Freeman, E., &amp; Freeman, E. (2021). Head first design patterns Second Edition. Sebastopol, CA: O'Reilly Media.
 - [Refactoring.guru](https://refactoring.guru/design-patterns)
--- src/com/designpattern/ekang/singleton/Singleton.java
+++ src/com/designpattern/ekang/singleton/Singleton.java
@@ -2,12 +2,16 @@ package com.designpattern.ekang.singleton;
 
 // ensure a class has only one instance, and provides a global point of access to it
 public class Singleton {
+    // static variable to hold one instance of the class Singleton
     private static Singleton uniqueInstance;
-    // make default constructor private, to prevent new operator
+    // constructor is declared private; only signleton can instantiate this class.
     private Singleton() {};
 
     public static synchronized Singleton getInstance() {
+        // uniqueInstance holds ONE instance and therefore if it is null, there is no created instance yet
         if (uniqueInstance == null)
+            // create instance via private constructor and assign it to uniqueInstance
+            // Note that if we never need the instance, it never gets created. == lazy instantiation
             uniqueInstance = new Singleton();
         return uniqueInstance;
     }
